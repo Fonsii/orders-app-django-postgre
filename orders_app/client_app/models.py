cb0ded64c7fc5dee44a74849172597ed88abfa3d
Fonsii <lualfonso14200174@gmail.com>
2022-09-05 14:18:24 -0600

    Admin register and models update

--- orders_app/client_app/models.py
+++ orders_app/client_app/models.py
@@ -3,8 +3,15 @@ from django.contrib.auth.models import User
 from store_app.models import ProductsStore, Store
 # Create your models here.
 
+STATE_CHOICES = (
+    ('REQUESTED', 'REQUESTED'),
+    ('ACCEPTED', 'ACCEPTED'),
+    ('REJECTED', 'REJECTED'),
+    ('COMPLETED', 'COMPLETED'),
+
+)
 class Order(models.Model):
     requester = models.ForeignKey(User, on_delete=models.CASCADE)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     product = models.ManyToManyField(ProductsStore)
-    state = models.IntegerField() # 0: pending, 1: accepted, 2: rejected, 3:completed
+    state = models.CharField(max_length=10, choices=STATE_CHOICES)
---
+++ orders_app/orders_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from store_app.models import Product, Tag
+
+admin.site.register(Product)
+admin.site.register(Tag)
